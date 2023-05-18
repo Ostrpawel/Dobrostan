765650efe6035a1b431773b5876eda0c28aba1df
OstrPawel <ostrowski.p84@gmail.com>
2023-05-18 22:57:37 +0200

    update js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.button');
+const allMenuItems = document.querySelectorAll('.menu__item')
 
 const handleNav = () => {
     menu.classList.toggle('menu--active')
+
+    allMenuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('menu--active')
+        })
+    })
 }
 
 menuBtn.addEventListener('click', handleNav)
