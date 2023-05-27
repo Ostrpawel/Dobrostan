66f59061ef1bc0d9274d0409012e96302ffabed0
OstrPawel <ostrowski.p84@gmail.com>
2023-05-27 22:29:29 +0200

    fix menu active

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@ const menu = document.querySelector('.menu')
 const menuBtn = document.querySelector('.button')
 const allMenuItems = document.querySelectorAll('.menu__item')
 
+allMenuItems.forEach(item => {
+	item.addEventListener('click', () => {
+		menu.classList.remove('menu--active')
+	})
+})
+
 const handleNav = () => {
 	menu.classList.toggle('menu--active')
 
-	allMenuItems.forEach(item => {
-		item.addEventListener('click', () => {
-			menu.classList.remove('menu--active')
-		})
-	})
 }
 
 menuBtn.addEventListener('click', handleNav)
